aced2f384d82db693de6981a2014b41b8f551e7b
Celine Davilmar <rdavi106@fiu.edu>
2022-07-29 23:39:58 -0400

    updates

--- portfolio-react/src/Components/Pages/Resume.js
+++ portfolio-react/src/Components/Pages/Resume.js
@@ -3,14 +3,45 @@ import React from 'react';
 export default function Resume() {
     return (
         <div>
-            <h1 class="title">Resume</h1>
+            <h1 class="title">Resume</h1><br />
             <p class="info-body">
-                Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-                Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-                mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-                lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-                in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+                <h2>Experience </h2>
+
+                <h4>Software Developer II</h4>
+                <h4>ETS</h4>
+                <h4>Mar 2022 – Present (5 mos)</h4>
+                <h4>Application Development</h4><br />
+
+                <h4>Software Specialist</h4>
+                <h4>CentralReach</h4>
+                <h4>Mar 2021 – Mar 2022 (1 yr 1 mo)</h4><br />
+
+
+                <h2>Education</h2>
+
+                <h4>University of Miami</h4>
+                <h4>Engineering</h4>
+                <h4>Mar 2022 – Aug 2022</h4><br />
+
+                <h4>Florida International University</h4>
+                <h4>Bachelor's degree</h4><br />
+
+
+                <h2>Licenses & certifications</h2>
+
+                <h4>JavaScript Algorithms and Data Structures - freeCodeCamp</h4>
+                <h4>Issued Feb 2022</h4><br />
+
+                <h2>Skills</h2>
+
+                <h4>Salesforce</h4>
+                <h4>Chrome Developer Tools</h4>
+                <h4>TypeScript</h4>
+                <h4>Jira</h4>
+                <h4>Gitlab</h4>
+                <h4>Confluence</h4>
+                <h4>Microsoft Office</h4>
+                <h4>Microsoft Excel</h4>
             </p>
         </div>
     );
