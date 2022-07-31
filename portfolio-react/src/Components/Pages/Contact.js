c38725600a2109e081f6283ee17c1e72a5adff53
Celine Davilmar <rdavi106@fiu.edu>
2022-07-30 21:00:24 -0400

    updates

--- portfolio-react/src/Components/Pages/Contact.js
+++ portfolio-react/src/Components/Pages/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
             <h1 class="title">Contact</h1>
             <div class="info-body">
                 <input class="contact" type="text" placeholder='Full Name' required />
-                <input class="contact" type="email" placeholder='Email@example.com' required />
+                <input class="contact" type="email" placeholder='Email@example.com' required formValidate />
                 <input class="contact-box" type="text" placeholder='Enter Message' required />
                 <button type="submit">Let's Connect!</button>
             </div>
